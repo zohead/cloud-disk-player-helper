a85e27885efa77ea0cbabca06543073d7bba26a5
zohead <nocwat@gmail.com>
2016-05-18 03:03:26 +0800

    Update to 0.3 version Fix video play window is not correctly opened bug on 360 cloud disk HTTPS page Tip user to redirect to HTTP video play page if needed Fix VXG video player window size bug after click play more than one time Update VXG video player js/pnacl files to 1.7.45 version

--- js/360-player.js
+++ js/360-player.js
@@ -3,15 +3,17 @@ var vxg_ext_id = "hncknjnnbahamgpjoafdebabmoamcnni";
 function vxg_play_video(vlink)
 {
 	var init = 0;
-	// replace flash player with VXG player if needed
-	if ($("#StrobeMediaPlayback").length > 0)
-		var replace_obj = $("#StrobeMediaPlayback");
-	else if ($("#main").length > 0)
-		var replace_obj = $("#main");
-	else
-		var replace_obj = null;
+	var replace_obj = null;
+	var vxg_cloud_player = vxgplayer('cloudplayerhelper_360_vxg_player');
 
-	if (replace_obj != null) {
+	// replace flash player with VXG player if needed
+	if (vxg_cloud_player) {
+	} else if ($("#StrobeMediaPlayback").length > 0)
+		replace_obj = $("#StrobeMediaPlayback");
+	else if ($("#main").length > 0)
+		replace_obj = $("#main");
+
+	if (replace_obj !== null) {
 		// initial player width and height are only useful when return from full-screen
 		var player_width = $("#main").outerWidth(true) - 30;
 		var player_height = $("#main").outerHeight(true) - 20;
@@ -22,7 +24,7 @@ function vxg_play_video(vlink)
 		$("#cloudplayerhelper_360_vxg_player").height("100%");
 	}
 
-	var vxg_cloud_player = vxgplayer('cloudplayerhelper_360_vxg_player');
+	vxg_cloud_player = vxgplayer('cloudplayerhelper_360_vxg_player');
 	if (vxg_cloud_player) {
 		if (init) {
 			vxg_cloud_player.onReadyStateChange(function(state){
@@ -33,6 +35,9 @@ function vxg_play_video(vlink)
 				// really show control bar
 				if ($("#cloudplayerhelper_360_vxg_player > .vxgplayer-controls").length > 0)
 					$("#cloudplayerhelper_360_vxg_player > .vxgplayer-controls").css("z-index", "1");
+				vxg_cloud_player.src(vlink);
+				vxg_cloud_player.play();
+				vxg_cloud_player.autoreconnect(1);
 			});
 		} else {
 			if (vxg_cloud_player.isPlaying())
@@ -56,6 +61,15 @@ function vxg_pause()
 	}
 }
 
+function check_https()
+{
+	if (window.location.protocol == "https:" && confirm(chrome.i18n.getMessage("clouddiskplayer_https_redirect"))) {
+		window.location.href = window.location.href.replace("https://", "http://");
+		return false;
+	} else
+		return true;
+}
+
 $(document).bind('DOMNodeInserted', function(e) {
 	if ($("#cloudplayerhelper_div").length <= 0) {
 		$("div.dl_app").before('<div id="cloudplayerhelper_div" style="display:inline; padding-left:40px;"><img id="cloudplayerhelper_icon" style="width:48px; height:48px; vertical-align:middle;" src="' + chrome.extension.getURL("icons/48.png") + '"/><span style="display:inline-block; padding-left:10px; height:48px; vertical-align:middle;"><a id="cloudplayerhelper_m3u8_link" href="" style="color:#FFFFFF;">[' + chrome.i18n.getMessage("clouddiskplayer_transcode") + ']</a>&nbsp;&nbsp;&nbsp;<a id="cloudplayerhelper_org_link" href="" style="color:#FFFFFF;">[' + chrome.i18n.getMessage("clouddiskplayer_original") + ']</a><span id="vxg_install_span" style="display:none;">&nbsp;&nbsp;&nbsp;<font color="#FFFFFF">(' + chrome.i18n.getMessage("clouddiskplayer_install") + ' <a href="https://chrome.google.com/webstore/detail/' + vxg_ext_id + '" target="_blank" style="color:#970000;">VXG Media Player</a> ' + chrome.i18n.getMessage("clouddiskplayer_install2") + ')</font></span></span></div>');
@@ -68,9 +82,11 @@ $(document).bind('DOMNodeInserted', function(e) {
 		document.head.removeChild(script);
 
 		$("#cloudplayerhelper_m3u8_link").click(function(e) {
+			if (!check_https()) return false;
 			return vxg_play_video(this.href);
 		});
 		$("#cloudplayerhelper_org_link").click(function(e) {
+			if (!check_https()) return false;
 			return vxg_play_video(this.href);
 		});
 		$("#cloudplayerhelper_icon").click(function(e) {
